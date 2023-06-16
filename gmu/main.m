c67e22f692af521b119f4ed928e46b37af56ee38
gurubac <gbrbachchu@gmail.com>
2023-06-16 16:35:42 -0700

    update

--- gmu/main.m
+++ gmu/main.m
@@ -6,11 +6,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "GamePolicyHeaders/GPProcessMonitor.h"
+#import "GamePolicyHeaders/GPProcessInfo.h"
+#import "GamePolicyHeaders/GPGameMonitorEnvoy.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        GPProcessMonitor *processMonitor = [GPProcessMonitor monitorForCurrentProcess];
+        // Check if process monitor is allocated properly
+        if (processMonitor != nil) {
+            NSLog(@"Process monitor allocated properly");
+        } else {
+            NSLog(@"Error: Process monitor allocation failed");
+        }
     }
     return 0;
 }
