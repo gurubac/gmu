b0668d7365cef5d0a7d0d3f4bb41da967dc2c9fc
gurubac <gbrbachchu@gmail.com>
2023-06-16 12:32:29 -0700

    init

---
+++ gmu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  gmu
+//
+//  Created by guru on 6/16/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
