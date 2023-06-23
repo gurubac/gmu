2d74f97dabdcf91492b11511738769892537dcfa
gurubac <gbrbachchu@gmail.com>
2023-06-23 12:58:02 -0700

    rm unused code

--- gmu/main.m
+++ gmu/main.m
@@ -2,19 +2,6 @@
 #import <objc/runtime.h>
 #import "GamePolicyHeaders/GPProcessMonitor.h"
 
-// Proxy class to act as a wrapper for GamePolicy.GamePolicyAppXPC interface
-@interface GamePolicyAppClientProxy : NSObject
-@end
-
-@implementation GamePolicyAppClientProxy
-+ (instancetype)GamePolicyAppClientInterface {
-    return [[self alloc] init];
-}
-
-// Implement any necessary methods from GamePolicy.GamePolicyAppXPC interface here
-// Example: - (void)method1 { }
-@end
-
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         GPProcessMonitor *processMonitor = [[GPProcessMonitor alloc] init];
